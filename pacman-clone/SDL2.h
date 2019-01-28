ba0bf33f61f392f53f39b8694713c3984761220a
Piotr <piotr.mamenas@gmail.com>
2019-01-28 21:56:21 +0100

    removing unique ptr sdl alloc, adding up simple shared ptr wrappers for sdl2 raw ptr's. Its not a rocket ship to do micro memory tuning

---
+++ pacman-clone/SDL2.h
@@ -0,0 +1,36 @@
+#ifndef SDLADAPTER_H
+#define SDLADAPTER_H
+
+#include <memory>
+#include <stdint.h>
+
+#include <SDL2/SDL.h>
+
+struct SDL_Deleter {
+    void operator()(SDL_Surface* ptr) { if (ptr) SDL_FreeSurface(ptr); }
+    void operator()(SDL_Texture* ptr) { if (ptr) SDL_DestroyTexture(ptr); }
+    void operator()(SDL_Renderer* ptr) { if (ptr) SDL_DestroyRenderer(ptr); }
+    void operator()(SDL_Window* ptr) { if (ptr) SDL_DestroyWindow(ptr); }
+};
+
+template<class T, class D = std::default_delete<T>>
+struct shared_ptr_with_deleter : public std::shared_ptr<T>
+{
+    explicit shared_ptr_with_deleter(T* t = nullptr)
+        : std::shared_ptr<T>(t, D()) {};
+
+    void reset(T* t = nullptr)
+    {
+        std::shared_ptr<T>::reset(t, D());
+    }
+};
+
+namespace SDL2
+{
+    using SurfaceSharedPtr = shared_ptr_with_deleter<SDL_Surface, SDL_Deleter>;
+    using TextureSharedPtr = shared_ptr_with_deleter<SDL_Texture, SDL_Deleter>;
+    using RendererSharedPtr = shared_ptr_with_deleter<SDL_Renderer, SDL_Deleter>;
+    using WindowSharedPtr = shared_ptr_with_deleter<SDL_Window, SDL_Deleter>;
+}
+
+#endif
