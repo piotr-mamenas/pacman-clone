808ffd02dda468b43a3da024aac66a76b0d12e26
Piotr <piotr.mamenas@gmail.com>
2019-01-28 22:42:28 +0100

    fixing linker error (sdl redefines main), init logic

--- pacman-clone/Main.cpp
+++ pacman-clone/Main.cpp
@@ -1,10 +1,14 @@
-#include "Main.h"
+#include <memory>
 #include "GameContext.h"
+
+#ifdef main
+#undef main
+#endif 
 
 int main(int argc, char* argv[])
 {
     bool isWindowMode = true;
-    std::shared_ptr<GameContext> context = std::make_shared<GameContext>(640, 480, isWindowMode);
+    std::shared_ptr<GameContext> context = std::make_shared<GameContext>(640,480,isWindowMode);
 
     bool isGameInitialized = context->init();
     if (!isGameInitialized)
